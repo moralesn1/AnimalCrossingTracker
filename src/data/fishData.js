e3f6cbac2bd98eff00a43a88c46c51775eeb5240
Nathaniel <nathanielmorales01@gmail.com>
2020-04-12 19:14:24 +1000

    added more data to fish data

--- src/data/fishData.js
+++ src/data/fishData.js
@@ -13,6 +13,13 @@ import softShelledTurtleImg from "../assets/fishImages/softshelledturtle.png";
 import snappingTurtleImg from "../assets/fishImages/snappingturtle.png";
 import tadpoleImg from "../assets/fishImages/tadpole.png";
 import frogImg from "../assets/fishImages/frog.png";
+import freshWaterGobyImg from "../assets/fishImages/freshwatergoby.png";
+import loachImg from "../assets/fishImages/loach.png";
+import catfishImg from "../assets/fishImages/catfish.png";
+import giantSnakeHeadImg from "../assets/fishImages/giantsnakehead.png";
+import bluegillImg from "../assets/fishImages/bluegill.png";
+import yellowPerchImg from "../assets/fishImages/yellowperch.png";
+import blackBassImg from "../assets/fishImages/blackbass.png";
 
 const fishData = [
   {
@@ -149,6 +156,69 @@ const fishData = [
     size: 2,
     time: "All Day",
     month: [1, 2, 11, 12],
+  },
+  {
+    image: freshWaterGobyImg,
+    name: "Fresh Water Goby",
+    price: 400,
+    location: "River",
+    size: 2,
+    time: "4PM to 9AM",
+    month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  },
+  {
+    image: loachImg,
+    name: "Loach",
+    price: 400,
+    location: "River",
+    size: 2,
+    time: "All Day",
+    month: [9, 10, 11],
+  },
+  {
+    image: catfishImg,
+    name: "Catfish",
+    price: 800,
+    location: "Pond",
+    size: 4,
+    time: "4PM to 9AM",
+    month: [1, 2, 3, 4, 11, 12],
+  },
+  {
+    image: giantSnakeHeadImg,
+    name: "Giant Snakehead",
+    price: 5500,
+    location: "Pond",
+    size: 5,
+    time: "9AM to 4PM",
+    month: [1, 2, 12],
+  },
+  {
+    image: bluegillImg,
+    name: "Bluegill",
+    price: 180,
+    location: "River",
+    size: 2,
+    time: "9AM to 4PM",
+    month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  },
+  {
+    image: yellowPerchImg,
+    name: "Yellow Perch",
+    price: 300,
+    location: "River",
+    size: 3,
+    time: "All Day",
+    month: [4, 5, 6, 7, 8, 9],
+  },
+  {
+    image: blackBassImg,
+    name: "Black Bass",
+    price: 400,
+    location: "River",
+    size: 4,
+    time: "All Day",
+    month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   },
 ];
 
