a5061472d62cff5cfdb1f6e8aeb2025de87b5618
Nathaniel <nathanielmorales01@gmail.com>
2020-06-16 18:01:55 +1000

    filter function search bar dynamic results

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,49 +5,51 @@ export default function Input() {
   const [display, setDisplay] = useState(false);
   const [search, setSearch] = useState("");
 
-  const fishList = (fish) => {
-    setDisplay(false);
-    console.log(fish);
-  };
-
   function handleChange(event) {
     const inputValue = event.target.value;
     setSearch(inputValue);
   }
 
-  function handleClick() {
-    setDisplay(!display);
-    setSearch("");
-  }
+  function toggleDisplay() {}
+  // const queryResult = fishData
+  //   .filter(({ name }) => name.toLocaleLowerCase().indexOf(search) > -1)
+  //   .map((item, index) => {
+  //     return (
+  //       <div className="option" key={index} id={item.name}>
+  //         <span>{item.name}</span>
+  //         <img src={item.image} alt={item.name}></img>
+  //       </div>
+  //     );
+  //   });
+
+  function addFish() {}
 
   return (
     <div className="form">
       <input
-        onClick={handleClick}
-        onChange={handleChange}
+        onInput={handleChange}
         type="text"
         placeholder="Search for fish..."
         title="Enter a fish name"
         value={search}
       />
-      {display && (
-        <div className="container">
-          {fishData
-            .filter(({ name }) => name.toLocaleLowerCase().indexOf(search) > -1)
-            .map((item, index) => {
-              return (
-                <div onClick={fishList} className="option" key={index}>
-                  <span>{item.name}</span>
-                  <img src={item.image} alt={item.name}></img>
-                </div>
-              );
-            })}
-        </div>
-      )}
-      <button type="submit">Add Fish</button>
-
+      <button type="submit" onClick={addFish}>
+        Add Fish
+      </button>
       <button>Clear List</button>
-      {/* {items.map((item) => (item === "" ? null : <li>{item}</li>))} */}
+
+      <div className="container">
+        {fishData
+          .filter(({ name }) => name.toLocaleLowerCase().indexOf(search) > -1)
+          .map((item, index) => {
+            return (
+              <div className="option" key={index} id={item.name}>
+                <span>{item.name}</span>
+                <img src={item.image} alt={item.name}></img>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
