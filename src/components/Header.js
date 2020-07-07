2eff0615a3fca25afb1c97472b0703de8acd2730
Nathaniel <nathanielmorales01@gmail.com>
2020-07-07 23:57:30 +1000

    add media query for screens below 400

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,17 @@ import { Container } from "react-bootstrap";
 export default function Header() {
   return (
     <Container className="header">
-      <div className="container">
-        <div className="hero-image-container">
-          <img
-            className="hero-image"
-            alt="animalcrossing-logo"
-            src={stitchestIMG}
-          />
-          <div className="header-title">
-            <h5>Inventory Critter Price Checker</h5>
-            <p>
-              Add your current inventory creatures to the list for their value.
-            </p>
-          </div>
+      <div className="hero-image-container">
+        <img
+          className="hero-image"
+          alt="animalcrossing-logo"
+          src={stitchestIMG}
+        />
+        <div className="header-title">
+          <h5>Inventory Critter Price Checker</h5>
+          <p>
+            Add your current inventory creatures to the list for their value.
+          </p>
         </div>
       </div>
     </Container>
